05e1dd0012edc1753ae7d8c2ee239e609fd1663f
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-20 02:49:59 -0500

    Parameterized routes

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  }
+];
+
 server.on('request', (req, res) => {
-  if (req.url === '/friends') {
-    // res.writeHead(200, {
-    //   'Content-Type': 'application/json',
-    // });
+  const items = req.url.split('/');
+  // /friends/2 => ['', 'friends', '2']
+  if (items[1] === 'friends') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-      id: 1,
-      name: 'Sir Isaac Newton',
-    }));
-  } else if (req.url === '/messages') {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === 'messages') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>');
