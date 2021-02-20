ea90f2fbe173c57f16b8b8e378ab0327433ec419
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-19 22:12:43 -0500

    Multiple endpoints

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-  res.end(JSON.stringify({
-    id: 1,
-    name: 'Sir Isaac Newton',
-  }));
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  if (req.url === '/friends') {
+    // res.writeHead(200, {
+    //   'Content-Type': 'application/json',
+    // });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+      id: 1,
+      name: 'Sir Isaac Newton',
+    }));
+  } else if (req.url === '/messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
