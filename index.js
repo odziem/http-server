58235f1dad21ab6d81ff490ade08d93af5ac7164
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-21 04:32:52 -0500

    POST to friends collection

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ const friends = [
 server.on('request', (req, res) => {
   const items = req.url.split('/');
   // /friends/2 => ['', 'friends', '2']
-  if (items[1] === 'friends') {
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log('Request:', friend);
+      friends.push(JSON.parse(friend));
+    })
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     if (items.length === 3) {
@@ -31,7 +37,7 @@ server.on('request', (req, res) => {
     } else {
       res.end(JSON.stringify(friends));
     }
-  } else if (items[1] === 'messages') {
+  } else if (req.method === 'GET' && items[1] === 'messages') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>');
